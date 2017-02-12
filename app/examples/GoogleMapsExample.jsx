0a7482d804dccae02f7aff6245522d86c006c1ab
Diego Borges <drborges.cic@gmail.com>
2017-02-12 01:31:20 -0200

    Update google maps example

--- app/examples/GoogleMapsExample.jsx
+++ app/examples/GoogleMapsExample.jsx
@@ -5,11 +5,18 @@ import GoogleMaps from '../components/google-maps'
 const GoogleMapsExample = (props) => (
   <Script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBccau1--crQs5SSMDYUwAZrmRszOeRiwQ">
     <GoogleMaps.Map
-      center={{ lat: -24.397, lng: 120.644 }}
-      scrollwheel={false}
-      zoom={8}
+      id="map"
+      center={{lat: 41.85, lng: -87.65}}
       onLoad={map => console.log(map)}
-    />
+      scrollwheel={false}
+      zoom={7}
+    >
+      <GoogleMaps.Directions
+        origin="chicago, il"
+        destination="oklahoma city, ok"
+        travelMode="DRIVING"
+      />
+  </GoogleMaps.Map>
   </Script>
 )
 
