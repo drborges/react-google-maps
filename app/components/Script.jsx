879f389ed048ee825dbaa0425744fda408276729
Diego Borges <drborges.cic@gmail.com>
2017-02-11 22:57:34 -0200

    Load children components only after Script has been loaded

--- app/components/Script.jsx
+++ app/components/Script.jsx
@@ -1,29 +1,57 @@
 import React from 'react'
 
 class Script extends React.Component {
+  state = { loaded: false }
+
+  static propTypes = {
+    async: React.PropTypes.bool,
+    charset: React.PropTypes.string,
+    crossorigin: React.PropTypes.string,
+    defer: React.PropTypes.bool,
+    type: React.PropTypes.string,
+    onLoad: React.PropTypes.func,
+    onError: React.PropTypes.func,
+    src: React.PropTypes.string.isRequired,
+  }
 
   static defaultProps = {
     id: "script-target",
     async: true,
     defer: true,
     type: "text/javascript",
+    onLoad: () => {},
+    onError: () => {},
+  }
+
+  handleLoad = (e) => {
+    this.setState({ loaded: true })
+    this.props.onLoad(e)
+  }
+
+  handleError = (e) => {
+    console.error(e)
+    this.props.onError(e)
   }
 
   componentDidMount() {
     const target = document.getElementById(this.props.id)
     const script = document.createElement("script")
-    script.onload = this.props.onLoad
-    script.onerror = this.props.onError
+    script.onload = this.handleLoad
+    script.onerror = this.handleError
     script.type = this.props.type
     script.async = this.props.async
     script.defer = this.props.defer
+    script.charset = this.props.charset
+    script.crossorigin = this.props.crossorigin
     script.setAttribute("src", this.props.src)
     target.appendChild(script)
   }
 
   render() {
     return (
-      <section id={this.props.id}></section>
+      <section id={this.props.id}>
+        {this.state.loaded ? this.props.children : null}
+      </section>
     )
   }
 }
--- app/examples/GoogleMapsExample.jsx
+++ app/examples/GoogleMapsExample.jsx
@@ -2,29 +2,15 @@ import React from 'react'
 import Script from '../components/Script'
 import GoogleMap from '../components/GoogleMap'
 
-class GoogleMapsExample extends React.Component {
-  state = { googleMapsLoaded: false }
-  endpoint = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBccau1--crQs5SSMDYUwAZrmRszOeRiwQ"
-
-  handleScriptLoad = () => {
-    this.setState({ googleMapsLoaded: true })
-  }
-
-  render() {
-    const loader = <Script src={this.endpoint} onLoad={this.handleScriptLoad} />
-    const maps = !this.state.googleMapsLoaded ? loader : (
+const GoogleMapsExample = (props) => {
+  return (
+    <Script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBccau1--crQs5SSMDYUwAZrmRszOeRiwQ">
       <GoogleMap
         center={{ lat: -24.397, lng: 120.644 }}
         scrollwheel={false}
         zoom={8} />
-    )
-
-    return (
-      <section className="google-maps-example">
-        {maps}
-      </section>
-    )
-  }
+    </Script>
+  )
 }
 
 export default GoogleMapsExample
