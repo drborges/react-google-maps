99597a7f2a5cb466d20816e3d63d9fc559c0609b
Diego Borges <drborges.cic@gmail.com>
2017-02-15 23:03:41 -0200

    Add second directions to the map

--- app/examples/GoogleMapsExample.jsx
+++ app/examples/GoogleMapsExample.jsx
@@ -5,15 +5,21 @@ import GoogleMaps from '../components/google-maps'
 const GoogleMapsExample = (props) => (
   <Script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBccau1--crQs5SSMDYUwAZrmRszOeRiwQ">
     <GoogleMaps.Map
-      id="map"
       center={{lat: 41.85, lng: -87.65}}
       onLoad={map => console.log(map)}
       scrollwheel={false}
       zoom={7}
     >
       <GoogleMaps.Directions
+        key={0}
         origin="chicago, il"
         destination="oklahoma city, ok"
+        travelMode="DRIVING"
+      />
+      <GoogleMaps.Directions
+        key={1}
+        origin="chicago, il"
+        destination="philadelphia, pa"
         travelMode="DRIVING"
       />
     </GoogleMaps.Map>
