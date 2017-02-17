89b5790966186668272bfd029ee00b365f4adc22
Diego Borges <drborges.cic@gmail.com>
2017-02-17 18:43:16 -0200

    Add waypoints to directions on the example

--- app/examples/GoogleMapsExample.jsx
+++ app/examples/GoogleMapsExample.jsx
@@ -2,32 +2,40 @@ import React from 'react'
 import Script from '../components/Script'
 import GoogleMaps from '../components/google-maps'
 
-const GoogleMapsExample = (props) => (
-  <Script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBccau1--crQs5SSMDYUwAZrmRszOeRiwQ">
-    <GoogleMaps.Map
-      center={{lat: 41.85, lng: -87.65}}
-      onLoad={map => console.log(map)}
-      scrollwheel={false}
-      zoom={7}
-    >
-      <GoogleMaps.Directions
-        key={0}
-        draggable={true}
-        origin="chicago, il"
-        destination="oklahoma city, ok"
-        travelMode="DRIVING"
-        onChange={directions => console.log(directions)}
-      />
-      <GoogleMaps.Directions
-        key={1}
-        draggable={true}
-        origin="chicago, il"
-        destination="philadelphia, pa"
-        travelMode="DRIVING"
-        onChange={directions => console.log(directions)}
-      />
-    </GoogleMaps.Map>
-  </Script>
-)
+const GoogleMapsExample = (props) => {
+  const waypoints = [
+    { location: "New York, NY", stopover: true },
+    { location: "Atlanta, GE",  stopover: true },
+  ]
+
+  return (
+    <Script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBccau1--crQs5SSMDYUwAZrmRszOeRiwQ">
+      <GoogleMaps.Map
+        center={{lat: 41.85, lng: -87.65}}
+        onLoad={map => console.log(map)}
+        scrollwheel={false}
+        zoom={7}
+      >
+        <GoogleMaps.Directions
+          key={0}
+          draggable={true}
+          origin="denver, co"
+          destination="san francisco, ca"
+          travelMode="DRIVING"
+        />
+
+        <GoogleMaps.Directions
+          key={1}
+          draggable={true}
+          waypoints={waypoints}
+          origin="chicago, il"
+          destination="oklahoma city, ok"
+          travelMode="DRIVING"
+          onChange={directions => console.log(directions)}
+        />
+      </GoogleMaps.Map>
+    </Script>
+  )
+}
 
 export default GoogleMapsExample
